1bc098433395d64e4d61ee06247e0d714b910c18
torihito <111257771+torihito@users.noreply.github.com>
2022-08-19 10:07:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-function addDarkmodeWidget() {
-  new Darkmode().showWidget();
-}
-window.addEventListener('load', addDarkmodeWidget);
-
 const options = {
-  time: '2s', // default: '0.3s'
-  label: '🌙', // default: ''
-  
+  bottom: '64px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: '暗', // default: ''
+  autoMatchOsTheme: true // default: true
 }
+ 
 
 const darkmode = new Darkmode(options);
 darkmode.showWidget();
